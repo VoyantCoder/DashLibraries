660448944e69fad4aa83c485ec7837c9c43ed2c0
KvinneKraft <KvinneKraft@protonmail.com>
2021-08-25 17:04:18 +0300

    Create Registry.cs

---
+++ Framework/class/b/Windows/System/Registry.cs
@@ -0,0 +1,33 @@
+﻿// Sector Name:
+// Author: Dashie
+
+
+#pragma warning disable IDE1006
+
+
+using System;
+using System.Linq;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+using DashFramework.DashResources;
+using DashFramework.ControlTools;
+using DashFramework.Runnables;
+using DashFramework.Erroring;
+using DashFramework.Sorters;
+using DashFramework.Data;
+
+
+namespace DashFramework
+{
+    namespace System
+    {
+	public class Registry
+	{
+
+	}
+    }
+}
