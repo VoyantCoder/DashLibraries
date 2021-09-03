472f19c08bd00c0439f0a7882695cfc36d3300d6
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-03 18:30:48 +0300

    Create Extension.cs

---
+++ Framework/class/b/Windows/Controls/DropMenu/CheckBoxDropMenu/Extension.cs
@@ -0,0 +1,71 @@
+﻿// Author: Dashie
+// Version: 1.0
+
+
+using System;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using DashFramework.DashControls.Customs;
+using DashFramework.DashControls;
+using DashFramework.ControlTools;
+using DashFramework.Sorters;
+using DashFramework.Data;
+
+
+namespace DashFramework
+{
+    namespace DashControls
+    {
+	namespace Customs
+	{
+	    public partial class CBDropMenu
+	    {
+		public void Show(bool force = false)
+		{
+		    try
+		    {
+			if (Layer1.Visible)
+			{
+			    if (!force)
+			    {
+				return;
+			    }
+			}
+
+			Layer1.Show();
+			Layer1.BringToFront();
+		    }
+
+		    catch
+		    {
+			throw;
+		    }
+		}
+
+		public void Hide(bool force = false)
+		{
+		    try
+		    {
+			if (!Layer1.Visible)
+			{
+			    if (!force)
+			    {
+				return;
+			    }
+			}
+
+			Layer1.Hide();
+		    }
+
+		    catch
+		    {
+			throw;
+		    }
+		}
+	    }
+	}
+    }
+}
