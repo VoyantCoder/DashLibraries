bf34f70b0d220c20bece5925a6168f6cf4afdb36
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-08 07:57:40 +0300

    Create Visibility.cs

---
+++ Framework/class/b/Windows/Controls/DropMenu/CheckBoxDropMenu/Visibility.cs
@@ -0,0 +1,36 @@
+﻿// Author: Dashie
+
+
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+
+namespace DashFramework
+{
+    namespace DashControls
+    {
+	namespace Customs
+	{
+	    public partial class CBDropMenu
+	    {
+		public void SetVisibilityTrigger(bool Show, params Control[] Objects)
+		{
+		    try
+		    {
+			foreach (Control Object in Objects)
+			{
+			    if (Show) Object.Enter += (s, e) => this.Show();
+			    else Object.Enter += (s, e) => Hide();
+			}
+		    }
+
+		    catch
+		    {
+			throw;
+		    }
+		}
+	    }
+	}
+    }
+}
