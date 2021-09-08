871314e986543c57c1e4e2819a3eca1d2d82e375
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-08 07:58:24 +0300

    test-code

--- Framework/class/a/MainGUI.cs
+++ Framework/class/a/MainGUI.cs
@@ -4,28 +4,46 @@
 
 using System;
 using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 using DashFramework.DashControls.Customs;
-using DashFramework.DashControls;
-using DashFramework.Erroring;
+using DashFramework.ControlTools;
+using DashFramework.Sorters;
 using DashFramework.Forms;
+
 
 namespace DashFramework
 {
     public partial class MainGUI
     {
-
 	public void Initiator(DashWindow inst)
 	{
 	    try
 	    {
-		// test framework components here.
+		CBDropMenu Menu = new CBDropMenu();
+		
+		Menu.Integrator(inst.Instance(), new Point(-1, -1), Color.Black,
+		    Color.FromArgb(64, 64, 64), Color.FromArgb(8, 8, 8), Color.White, 200, 
+		    ("I fluff", () => MessageBox.Show("!")), ("I also fluff", () => MessageBox.Show("!!")),
+		    ("I fluff also", () => MessageBox.Show("!!!")), ("I also flufferyyyy", () => MessageBox.Show("!!!!")),
+		    ("I flufadadadff", () => MessageBox.Show("!!!!!")), ("I alrrrrrrso fluff", () => MessageBox.Show("!!!!!!")));
+
+		Color Hover = Color.FromArgb(48, 48, 48);
+		Color Leave = Color.FromArgb(0, 0, 0);
+		Color Down = Color.FromArgb(32, 32, 32);
+		Color Up = Color.FromArgb(48, 48, 48);
+		
+		Menu.SetEventColor(Hover, MenuBarColor.License, MenuBarColorEvent.Enter);
+		Menu.SetEventColor(Leave, MenuBarColor.License, MenuBarColorEvent.Leave);
+		Menu.SetEventColor(Down, MenuBarColor.License, MenuBarColorEvent.Down);
+		Menu.SetEventColor(Up, MenuBarColor.License, MenuBarColorEvent.Up);
 	    }
 
-	    catch (Exception E)
+	    catch
 	    {
-		ErrorHandler.GetException(E);
+		throw;
 	    }
 	}
     }
