4d187925b8ae8958d8de6d524aa321acbb27d324
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-08 05:25:27 +0300

    Create InsertyThread.cs

---
+++ Framework/class/b/Threading/InsertyThread.cs
@@ -0,0 +1,74 @@
+﻿// Author: Dashie
+
+using System;
+using System.Threading;
+
+namespace DashFramework
+{
+    namespace Threading
+    {
+	public class InsertyThread
+	{
+	    Thread MainThread = default(Thread);
+	    Action Code = default(Action);
+
+	    delegate void ThreadRunnable();
+
+	    void InsertCode(Action Code)
+	    {
+		try
+		{
+		    this.Code = Code;
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+
+	    void StartThread(Action Optional = default(Action))
+	    {
+		try
+		{
+		    MainThread = new Thread(() =>
+		    {
+			try
+			{
+			    if (Optional != default(Action))
+			    {
+				Code();
+			    }
+
+			    do
+			    {
+				if (Code != default(Action))
+				{
+				    Code.Invoke();
+				    Code = default(Action);
+				}
+			    }
+
+			    while (true);
+			}
+
+			catch
+			{
+			    throw;
+			}
+		    });
+
+		    MainThread.Start();
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+
+	    public InsertyThread(Action Code) => StartThread(Code);
+	    public InsertyThread() => StartThread();
+	}
+    }
+}
