2f232a5d512b4b896217d229bf44c510f009ce45
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-13 22:28:50 +0300

    refurbished old functionality

--- Framework/class/b/Networking/Networking.cs
+++ Framework/class/b/Networking/Networking.cs
@@ -17,157 +17,36 @@ namespace DashFramework
 {
     namespace Networking
     {
-	public class Network
+	public partial class Network
 	{
-	    private void HandleError(Exception E) => ErrorHandler.JustDoIt(E);
-
-
-	    public bool AllowedDomain(string data) => !new List<string>()
-		{ ".gov", ".govt", ".edu" }.Any(data.EndsWith);
-
-
-	    public bool CanInteger(string data) => GetInteger(data) != -1;
-	    public bool CanPort(string data) => (GetPort(data) != -1);
-	    public bool CanByte(string data) => CanDuration(data);
-	    public bool CanIP(string data) => GetIP(data) != string.Empty;
-
-
-	    public bool CanDuration(string data)
-	    {
-		int duration = GetInteger(data);
-		return (duration != 1 && duration >= 10);
-	    }
-
-
-	    public int GetInteger(string data)
-	    {
-		try
-		{
-		    return int.Parse(data);
-		}
-
-		catch
-		{
-		    return -1;
-		}
-	    }
-
-
-	    public string GetIP(string data)
-	    {
-		try
-		{
-		    var r_host = data.ToLower();
-
-		    if (!IPAddress.TryParse(r_host, out IPAddress ham))
-		    {
-			if (!r_host.Contains("http://") && !r_host.Contains("https://"))
-			{
-			    r_host = "https://" + r_host;
-			}
-
-			if (!Uri.TryCreate(r_host, UriKind.RelativeOrAbsolute, out Uri bacon))
-			{
-			    return string.Empty;
-			}
-
-			try
-			{
-			    r_host = Dns.GetHostAddresses(bacon.Host)[0].ToString();
-			}
-
-			catch
-			{
-			    return string.Empty;
-			}
-		    }
-
-		    else
-		    {
-			r_host = ham.ToString();
-
-			if (ham.AddressFamily != AddressFamily.InterNetwork && ham.AddressFamily != AddressFamily.InterNetworkV6)
-			{
-			    return string.Empty;
-			}
-		    }
-
-		    if (r_host.Length < 7 || r_host == string.Empty)
-		    {
-			return string.Empty;
-		    }
-
-		    return r_host;
-		}
-
-		catch
-		{
-		    return string.Empty;
-		}
-	    }
-
-
-	    public AddressFamily GetAddressFamily(string data)
-	    {
-		try
-		{
-		    return IPAddress.Parse(data).AddressFamily;
-		}
-
-		catch (Exception E)
-		{
-		    throw (ErrorHandler.GetException(E));
-		}
-	    }
-
-
 	    public bool IsHostReachable(string host, int port = 80, SocketType socketType = SocketType.Stream, ProtocolType protocol = ProtocolType.Tcp, string packetData = ".", int timeout = 500)
 	    {
 		try
 		{
-		    using (Socket socket = new Socket(GetAddressFamily(host), socketType, protocol)
-		    { LingerState = new LingerOption(true, 0) })
-		    {
-			IAsyncResult socketResult = socket.BeginConnect(host, port, null, null);
-			bool socketSuccess = socketResult.AsyncWaitHandle.WaitOne(timeout, true);
+		    using (Socket socket = new Socket(GetAddrFam(host), socketType, protocol)
+		    { LingerState = new LingerOption(true, 0) });
 
-			if (packetData != ".")
+			if (true)
 			{
-			    socket.Send(Encoding.ASCII.GetBytes(packetData), SocketFlags.None);
+			    IAsyncResult socketResult = socket.BeginConnect(host, port, null, null);
+			    bool socketSuccess = socketResult.AsyncWaitHandle.WaitOne(timeout, true);
+
+			    if (packetData != ".")
+			    {
+				socket.Send(Encoding.ASCII.GetBytes(packetData), SocketFlags.None);
+			    }
+
+			    bool connected = socket.Connected;
+
+			    socket.Disconnect(false);
+
+			    return connected;
 			}
-
-			bool connected = socket.Connected;
-
-			socket.Disconnect(false);
-
-			return connected;
-		    }
 		}
 
 		catch
 		{
 		    return false;
-		}
-	    }
-
-
-	    public int GetPort(string data)
-	    {
-		try
-		{
-		    int iData = GetInteger(data);
-
-		    if (iData == -1 || iData < 0 || iData > 65535)
-		    {
-			return -1;
-		    }
-
-		    return iData;
-		}
-
-		catch
-		{
-		    return -1;
 		}
 	    }
 	}
