b2142c5e88c957b3bbae119b8a5cf7f8cdfa44d6
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-14 18:18:28 +0300

    Create PortScan.cs

---
+++ Framework/class/b/Networking/PortScan.cs
@@ -0,0 +1,17 @@
+﻿
+// Author: Dashie
+
+
+namespace DashFramework
+{
+    namespace Networking
+    {
+	public partial class Network
+	{ 
+	    // IEnumerable<bool> ScanPorts(address(es), port(s), timeout, packetData)
+	    // {
+	    //
+	    // }
+	}
+    }
+}
