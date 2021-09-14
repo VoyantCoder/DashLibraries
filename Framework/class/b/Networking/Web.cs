9363d9b23f6ddbe64537c1cf94c8465cde255da5
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-14 18:18:30 +0300

    Create Web.cs

---
+++ Framework/class/b/Networking/Web.cs
@@ -0,0 +1,17 @@
+﻿
+// Author: Dashie
+
+
+namespace DashFramework
+{
+    namespace Networking
+    {
+	public partial class Network
+	{
+	    // Header Validator (check whether failed to arrive or not)
+	    // Random User-Agent Generator (store file with user-agents)
+	    // Random Header Generator (should have settings)
+	    // HTTP Server (set a directory etc etc)
+	}
+    }
+}
