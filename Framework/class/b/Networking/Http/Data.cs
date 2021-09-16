f6cbf086c3296bdf0c22c802620d39d895aef2e2
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-16 21:38:26 +0300

    added comments

---
+++ Framework/class/b/Networking/Http/Data.cs
@@ -0,0 +1,21 @@
+﻿
+// Author: Dashie
+
+
+namespace DashFramework
+{
+    namespace Networking
+    {
+	public partial class Network
+	{
+	    public partial class HttpServer
+	    {
+		// Random User-Agent Gen -- generates a random user-agent
+		// Random Header Gen -- generates a random header
+		// Header Generator -- generates a header based on options set
+		// SendHttpHeader -- sends http header and gets the response return value
+		// Header Validator -- validates given headers using local server
+	    }
+	}
+    }
+}
