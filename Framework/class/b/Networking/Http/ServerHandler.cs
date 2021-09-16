17f0471d39200902e0a9684fee528cc5fce3d98d
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-16 21:38:21 +0300

    removed comments

--- Framework/class/b/Networking/Http/ServerHandler.cs
+++ Framework/class/b/Networking/Http/ServerHandler.cs
@@ -113,9 +113,6 @@ namespace DashFramework
 		    }
 		}
 	    }
-	    // Header Validator (check whether failed to arrive or not)
-	    // Random User-Agent Generator (store file with user-agents)
-	    // Random Header Generator (should have settings)
 	}
     }
 }
