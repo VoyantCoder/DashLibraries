6a220ba47c3fd94e09391c4919971ff93e1b5679
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-21 20:49:51 +0300

    timers class finished
    allows you to display the notification for a given amount of miliseconds.  currently lags a bit, why?  I have yet to find out.  The code does the exact same as my previous code, just a lot faster.  It makes no sense.  Instead of 8 separate method calls, just one, yet it acts as if I am generating a thousand characters using the += operator.

---
+++ Framework/class/b/Windows/Controls/Other/Notification/Timer.cs
@@ -0,0 +1,136 @@
+﻿
+// Author: Dashie
+
+
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+using DashFramework.Runnables;
+
+
+namespace DashFramework
+{
+    namespace DashControls.Controls
+    {
+	public partial class Notification
+	{
+	    readonly System.Timers.Timer timer = new System.Timers.Timer() { AutoReset = false, Enabled = false, };
+	    readonly Runnable runnable = new Runnable();
+
+	    public void EnableAppearanceTimer(int Delay)
+	    {
+		try
+		{   
+		    if (timer.Enabled)
+		    {
+			timer.Enabled = false;
+		    }
+
+		    timer.Elapsed += (s, e) =>
+		    {
+			try
+			{
+			    new Thread(() =>
+			    {
+				try
+				{
+				    void sync(Action Code)
+				    {
+					try
+					{
+					    runnable.RunTaskSynchronously(Panel1.Parent, () =>
+					    {
+						Code();
+					    });
+					}
+
+					catch
+					{
+					    throw;
+					}
+				    }
+
+				    void recolor(Control control, int alpha)
+				    {
+					try
+					{
+					    Color color = Color.FromArgb
+					    (
+						alpha,
+						backColor.R,
+						backColor.G,
+						backColor.B
+					    );
+
+					    sync(() => control.BackColor = color);
+					}
+
+					catch
+					{
+					    throw;
+					}
+				    }
+
+				    for (int a = 255; a >= 30; a -= 5)
+				    {//Slow, why?  Idek
+					recolor(Panel1, a);
+					recolor(Label1, a);
+					recolor(Label2, a);
+					
+					Thread.Sleep(50);
+				    }
+				    
+				    sync(() => Panel1.Hide());
+
+				    recolor(Panel1, 254);
+				    recolor(Label1, 254);
+				    recolor(Label2, 254);
+				}
+
+				catch
+				{
+				    throw;
+				};
+
+				timer.Enabled = false;
+			    })
+
+			    { IsBackground = true }.Start();
+			}
+
+			catch
+			{
+			    throw;
+			}
+		    };
+
+		    timer.Interval = Delay;
+		    timer.Enabled = true;
+
+		    Panel1.VisibleChanged += (s, e) =>
+		    {
+			try
+			{
+			    if (Panel1.Visible)
+			    {
+				timer.Start();
+			    }
+			}
+
+			catch
+			{
+			    throw;
+			}
+		    };
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+	}
+    }
+}
