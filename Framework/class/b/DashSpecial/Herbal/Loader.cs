8413b62a33993378da16039e1d71b442c5d492ef
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-22 21:00:08 +0300

    Create Loader.cs

---
+++ Framework/class/b/DashSpecial/Herbal/Loader.cs
@@ -0,0 +1,46 @@
+﻿
+// Author: Dashie
+
+
+using System;
+using System.Collections.Generic;
+
+
+namespace DashFramework
+{
+    namespace DashSpecial.Herbal
+    {
+	public struct WeedStrain
+	{
+	    public string StrainName;
+	    public int Weeks;
+
+	    public double IndicaContents;
+	    public double SativaContents;
+	    public double Price;
+
+	    public bool Autoflower;
+	    public bool Feminised;
+	}
+
+	public partial class Weeds
+	{
+	    public WeedStrain[] GetStrains()
+	    {
+		List<WeedStrain> strains = new List<WeedStrain>();
+
+		try
+		{
+		    // Configuration file processing
+		}
+
+		catch
+		{
+		    throw;
+		}
+
+		return strains.ToArray();
+	    }
+	}
+    }
+}
