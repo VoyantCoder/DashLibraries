8286b65d337354c593969d6bbfa6d715f96a210f
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-23 15:19:25 +0300

    a class for string extension methods

---
+++ Framework/class/b/NETExt/string.cs
@@ -0,0 +1,30 @@
+﻿
+// Author: Dashie
+
+
+using System;
+
+
+namespace DashFramework
+{
+    namespace NETExtensions
+    {
+	public static class StringExtension
+	{
+	    public static string[] ISplit(this string inst, params char[] chars)
+	    {
+		return inst.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+	    }
+
+	    public static string RemoveAll(this string inst, params string[] crits)
+	    {
+		foreach (string crit in crits)
+		{
+		    inst = inst.Replace(crit, "");
+		}
+
+		return inst;
+	    }
+	}
+    }
+}
