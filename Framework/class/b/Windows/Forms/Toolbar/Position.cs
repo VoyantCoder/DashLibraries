26ce8c9cdc74e2f2ceed28e1deba1111654ebdb4
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-25 18:38:39 +0300

    moved

---
+++ Framework/class/b/Windows/Forms/Toolbar/Position.cs
@@ -0,0 +1,117 @@
+﻿
+// Author: Dashie
+
+
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace DashFramework
+{
+    namespace Forms
+    {
+	public partial class Toolbar
+	{
+	    public void SetLocation(Point Location)
+	    {
+		try
+		{
+		    Panel1.Location = Location;
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+
+	    public Point GetPosition(ToolbarPosition Position, Control Parent = default(Control), Size Size = default(Size))
+	    {
+		try
+		{
+		    if (Parent == default(Control))
+		    {
+			if (Panel1.Parent == null)
+			{
+			    return Point.Empty;
+			}
+
+			Parent = Panel1.Parent;
+		    }
+
+		    if (Size == default(Size))
+		    {
+			if (Panel1.Parent == null)
+			{
+			    return Point.Empty;
+			}
+
+			Size = Panel1.Size;
+		    }
+
+		    Point Location = new Point(-2, -2);
+
+		    switch (Position)
+		    {
+			case ToolbarPosition.Top:
+			{
+			    Location.Y = 0;
+			    break;
+			}
+
+			case ToolbarPosition.Bottom:
+			{
+			    Location.Y = Parent.Height - Size.Height;
+			    break;
+			}
+
+			case ToolbarPosition.Right:
+			{
+			    Location.X = Parent.Width - Size.Width;
+			    break;
+			}
+
+			case ToolbarPosition.Left:
+			{
+			    Location.X = 0;
+			    break;
+			}
+		    }
+
+		    return Location;
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+
+	    public void SetPosition(ToolbarPosition Position)
+	    {
+		try
+		{
+		    SetLocation(GetPosition(Position, null, Size.Empty));
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+
+	    public void Integrate(Control Parent, Color BackColor, Size Size, ToolbarPosition Position)
+	    {
+		try
+		{
+		    Integrate(Parent, BackColor, Size, GetPosition(Position, Parent, Size));
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+	}
+    }
+}
---
+++ Framework/class/b/Windows/Forms/Toolbar/Values.cs
@@ -0,0 +1,46 @@
+﻿
+// Author: Dashie
+
+
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using DashFramework.DashControls.Controls;
+using DashFramework.DashControls;
+using DashFramework.ControlTools;
+using DashFramework.Sorters;
+
+
+namespace DashFramework
+{
+    namespace Forms
+    {
+	public enum ToolbarPosition
+	{
+	    Top, Bottom, Right, Left, Center
+	}
+
+	public enum ToolbarTemplate
+	{
+	    SaveLoadOpen, OkayCancel, Close
+	}
+
+	public partial class Toolbar
+	{
+	    readonly Dictionary<int, Control> ChildCollection = new Dictionary<int, Control>();
+	    readonly List<Button> ButtonCache = new List<Button>();
+
+	    readonly DashPanel Panel1 = new DashPanel();
+	    readonly DashPanel Panel2 = new DashPanel();
+
+	    readonly Simplifiers.Quickify QuickyInjector = new Simplifiers.Quickify();
+	    readonly ControlIntegrator Integrator = new ControlIntegrator();
+
+	    readonly Transformer Transform = new Transformer();
+	    readonly PlainSorters Sorters = new PlainSorters();
+
+	    readonly Apply Appliance = new Apply();
+	    public delegate void ChildRun();
+	}
+    }
+}
