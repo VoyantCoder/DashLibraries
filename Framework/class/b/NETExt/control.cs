d9edcd691424c1d2c0d3944ac11ca76d4bb66917
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-27 05:08:56 +0300

    added extension methods
    for controls

---
+++ Framework/class/b/NETExt/control.cs
@@ -0,0 +1,69 @@
+﻿
+// Author: Dashie
+
+
+using System;
+using System.Windows.Forms;
+
+using DashFramework.ControlTools;
+
+
+namespace DashFramework
+{
+    namespace NETExtensions
+    {
+	public static class ControlExtension
+	{
+	    readonly static Extern ext = new Extern();
+
+	    public static void SetUrl(this Control i, Control control, string dest)
+	    {
+		try
+		{
+		    control.Click += (s, e) =>
+		    {
+			try
+			{
+			    ext.OpenUrl(dest);
+			}
+
+			catch
+			{
+			    throw;
+			}
+		    };
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+
+
+	    public static void RegisterClickEvent(this Control i, Control control, Action code)
+	    {
+		try
+		{
+		    control.Click += (s, e) =>
+		    {
+			try
+			{
+			    code();
+			}
+
+			catch
+			{
+			    throw;
+			}
+		    };
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+	}
+    }
+}
