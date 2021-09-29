2e56431461684dbf6d01aec461e114a326a22b4a
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-30 01:23:47 +0300

    SetWindowSize method added

--- Framework/class/b/Windows/Forms/DashWindow/Unnamed.cs
+++ Framework/class/b/Windows/Forms/DashWindow/Unnamed.cs
@@ -65,6 +65,23 @@ namespace DashFramework
 		}
 	    }
 
+	    public void SetWindowSize(Size size)
+	    {
+		try
+		{
+		    Size menubarSize = new Size(size.Width, 
+			Menubar().GetMenubarSize().Height);
+
+		    Menubar().SetMenubarSize(menubarSize);
+		    Transform.Resize(WindowInstance, size);
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+
 	    public void SetWindowBackColor(Color Color)
 	    {
 		try
