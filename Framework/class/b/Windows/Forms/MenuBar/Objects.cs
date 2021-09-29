c5de91ded6664eaebc96847a09838acdeecc834d
KvinneKraft <KvinneKraft@protonmail.com>
2021-09-29 23:17:23 +0300

    restructured

--- Framework/class/b/Windows/Forms/MenuBar/Objects.cs
+++ Framework/class/b/Windows/Forms/MenuBar/Objects.cs
@@ -37,27 +37,24 @@ namespace DashFramework
 
 	public partial class MenuBar
 	{
-	    public int BorderRadius = 8;
-	    public int FontPoints = 10;
-	    public int FontTypeId = 1;
-
-	    readonly List<Button> CustomButtons = new List<Button>();
-
-	    readonly PictureBox Icon1 = new PictureBox();
-	    readonly PictureBox Icon2 = new PictureBox();
-
-	    readonly DashPanel Panel1 = new DashPanel();
-
-	    readonly Button Button1 = new Button();
-	    readonly Button Button2 = new Button();
-
-	    readonly Label Label1 = new Label();
-
 	    readonly ControlIntegrator Integrator = new ControlIntegrator();
 	    readonly ResourceTools ResourceTool = new ResourceTools();
 	    readonly Transformer Transform = new Transformer();
 	    readonly GenericSorter Sort = new GenericSorter();
 	    readonly Appliance Appliance = new Appliance();
+
+	    readonly List<Button> CustomButtons = new List<Button>();
+
+	    readonly PictureBox Icon1 = new PictureBox();
+	    readonly PictureBox Icon2 = new PictureBox();
+	    readonly DashPanel Panel1 = new DashPanel();
+	    readonly Button Button1 = new Button();
+	    readonly Button Button2 = new Button();
+	    readonly Label Label1 = new Label();
+
+	    public int BorderRadius = 8;
+	    public int FontPoints = 10;
+	    public int FontTypeId = 1;
 	}
     }
 }
