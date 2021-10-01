53017c6348d4808f9a59d50e482b0fe9bbe165b2
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-01 20:45:28 +0300

    experimental class

---
+++ Framework/class/b/Windows/Controls/Other/TransLabel/Constructor.cs
@@ -0,0 +1,31 @@
+﻿
+// Author: Dashie
+
+
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace DashFramework
+{
+    namespace DashControls.Controls
+    {
+	public partial class TransLabel
+	{
+	    public TransLabel()
+	    {
+		try
+		{
+		    SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+		    SetStyle(ControlStyles.Opaque, true);
+		    BackColor = Color.Transparent;
+		}
+
+		catch
+		{
+		    throw;
+		}
+	    }
+	}
+    }
+}
