0f0f06f3e51f8f2687709e2130929307ac2b9e53
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-08 22:36:49 +0300

    refactor

---
+++ Framework/class/b/Windows/Controls/Integration/Additional/Caching.cs
@@ -0,0 +1,99 @@
+﻿
+// Author: Dashie
+
+
+using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using DashFramework.DashControls.Controls;
+
+
+namespace DashFramework
+{
+    namespace DashControls
+    {
+        public partial class ControlIntegrator
+        {
+            private readonly Dictionary<Control, List<Control>> Controls = new Dictionary<Control, List<Control>>();
+
+            public IEnumerable<Control> GetControls(Control parent, params Type[] type)
+            {
+                if (!Controls.ContainsKey(parent))
+                {
+                    yield return null;
+                    yield break;
+                }
+
+                var types = type.ToList();
+
+                foreach (Control control in Controls[parent])
+                {
+                    if (types.Contains(control.GetType()))
+                    {
+                        yield return control;
+                    }
+                }
+            }
+
+            private void Register(Control parent, Control child)
+            {
+                try
+                {
+                    if (!Controls.ContainsKey(parent))
+                    {
+                        Controls.Add(parent, new List<Control>());
+                    }
+
+                    Controls[parent].Add(child);
+                }
+
+                catch
+                {
+                    throw;
+                }
+            }
+
+            private void UpdateRegister(Control parent, Control control)
+            {
+                try
+                {
+                    if (Controls.ContainsKey(parent))
+                    {
+                        if (Controls[parent].Contains(control))
+                        {//only works for first occurrence, but why occur twice?
+                            int id = Controls[parent].IndexOf(control);
+                            Controls[parent][id] = control;
+                        }
+                    }
+                }
+
+                catch
+                {
+                    throw;
+                }
+            }
+
+            private readonly Dictionary<Control, List<DashCheckBox>> Checkboxes = new Dictionary<Control, List<DashCheckBox>>();
+
+            private void Register(Control parent, DashCheckBox checkBox)
+            {
+                try
+                {
+                    if (!Checkboxes.ContainsKey(parent))
+                    {
+                        Checkboxes.Add(parent, new List<DashCheckBox>());
+                    }
+
+                    Checkboxes[parent].Add(checkBox);
+                }
+
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ Framework/class/b/Windows/Controls/Integration/Additional/Objects.cs
@@ -0,0 +1,28 @@
+﻿
+// Author: Dashie
+
+
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using DashFramework.DashResources;
+using DashFramework.ControlTools;
+using DashFramework.Sorters;
+using DashFramework.Data;
+
+
+namespace DashFramework
+{
+    namespace DashControls
+    {
+	public partial class ControlIntegrator
+	{
+	    public readonly Dictionary<TextBox, int> TextBoxContainers = new Dictionary<TextBox, int>();
+
+	    readonly ResourceTools ResourceTool = new ResourceTools();
+	    readonly Transformer Transform = new Transformer();
+	    readonly GenericSorter Sort = new GenericSorter();
+	    readonly DataTools DataTool = new DataTools();
+	}
+    }
+}
