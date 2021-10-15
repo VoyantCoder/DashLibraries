305ab7a3134c3322e0a23e25b19ba6e4557de9ea
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-15 11:42:30 +0300

    Window, Parent and Menubar properties added

---
+++ Framework/class/b/Windows/Forms/HelperGUI/additional/Properties.cs
@@ -0,0 +1,36 @@
+﻿
+// Author: Dashie
+
+
+namespace DashFramework
+{
+    namespace Forms
+    {
+        public partial class HelperGUI
+        {
+            public MenuBar Menubar
+            {
+                get
+                {
+                    return WindowInst.Menubar();
+                }
+            }
+
+            public DashWindow Window
+            {
+                get
+                {
+                    return WindowInst;
+                }
+            }
+
+            public Form Parent
+            {
+                get
+                {
+                    return WindowInst.Instance;
+                }
+            }
+        }
+    }
+}
