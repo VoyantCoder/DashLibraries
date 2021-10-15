8be6c7993416d0af440940a626bdcee7235f1bb8
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-15 11:33:39 +0300

    reformat
    Reformat of the file, because VS2019 hates my indentations.  Also, inactivity was due to study, obtaining information tends to be more useful than expressing it.  Regardless, this is one of the many commits.  Many files were reformatted.  RIP me.

--- Framework/class/b/DashSpecial/Herbal/Loader.cs
+++ Framework/class/b/DashSpecial/Herbal/Loader.cs
@@ -16,187 +16,187 @@ namespace DashFramework
 {
     namespace DashSpecial.Herbal
     {
-	public struct WeedStrain
-	{
-	    public string StrainName;
-	    public string Crossings;
-	    public int Weeks;
+        public struct WeedStrain
+        {
+            public string StrainName;
+            public string Crossings;
+            public int Weeks;
 
-	    public double IndicaContents;
-	    public double SativaContents;
-	    public double Price;
+            public double IndicaContents;
+            public double SativaContents;
+            public double Price;
 
-	    public bool Autoflower;
-	    public bool Feminised;
-	}
-	
-	public partial class Weeds
-	{
-	    string[] getFileContents()
-	    {
-		return File.ReadAllText("strains.dash").RemoveAll("(").ISplit(')');
-	    }
+            public bool Autoflower;
+            public bool Feminised;
+        }
 
-	    public IEnumerable<string[]> TranslateHerbBlocks()
-	    {
-		string[] blocks = getFileContents();
+        public partial class Weeds
+        {
+            string[] getFileContents()
+            {
+                return File.ReadAllText("strains.dash").RemoveAll("(").ISplit(')');
+            }
 
-		for (int k = 0; k < blocks.Length; k += 1)
-		{
-		    string[] sections = blocks[k].ISplit('\r', '\n');
+            public IEnumerable<string[]> TranslateHerbBlocks()
+            {
+                string[] blocks = getFileContents();
 
-		    for (int s = 0, l = 0; s < sections.Length; s += 1, l = 0)
-		    {
-			void Update(int i)
-			{
-			    l = i;
-			}
+                for (int k = 0; k < blocks.Length; k += 1)
+                {
+                    string[] sections = blocks[k].ISplit('\r', '\n');
 
-			switch (s)
-			{
-			    case 0: Update(11); break;
-			    case 1: Update(10); break;
-			    case 2: Update(15); break;
-			    case 3: Update(15); break;
-			    case 4: Update(13); break;
-			    case 5: Update(12); break;
-			    case 6: Update(10); break;
-			    case 7: Update(19); break;
-			}
+                    for (int s = 0, l = 0; s < sections.Length; s += 1, l = 0)
+                    {
+                        void Update(int i)
+                        {
+                            l = i;
+                        }
 
-			if (l == 0)
-			{
-			    MessageBox.Show("!!");
-			    yield return null;
-			    yield break;
-			}
+                        switch (s)
+                        {
+                            case 0: Update(11); break;
+                            case 1: Update(10); break;
+                            case 2: Update(15); break;
+                            case 3: Update(15); break;
+                            case 4: Update(13); break;
+                            case 5: Update(12); break;
+                            case 6: Update(10); break;
+                            case 7: Update(19); break;
+                        }
 
-			sections[s] = sections[s].Remove(0, l);
-			sections[s] = sections[s].StartsWith(" ")
-			    ? sections[s].Remove(0, 1) : sections[s];
+                        if (l == 0)
+                        {
+                            MessageBox.Show("!!");
+                            yield return null;
+                            yield break;
+                        }
 
-			if (sections.Length != 8)
-			{
-			    MessageBox.Show("!!!");
-			    yield return null;
-			    yield break;
-			}
-		    }
+                        sections[s] = sections[s].Remove(0, l);
+                        sections[s] = sections[s].StartsWith(" ")
+                            ? sections[s].Remove(0, 1) : sections[s];
 
-		    bool IsDouble(string s)
-		    {
-			try
-			{
-			    double.Parse(s);
-			}
+                        if (sections.Length != 8)
+                        {
+                            MessageBox.Show("!!!");
+                            yield return null;
+                            yield break;
+                        }
+                    }
 
-			catch
-			{
-			    return false;
-			}
-			return true;
-		    }
+                    bool IsDouble(string s)
+                    {
+                        try
+                        {
+                            double.Parse(s);
+                        }
 
-		    bool IsInt(string s)
-		    {
-			try
-			{
-			    int.Parse(s);
-			}
+                        catch
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
 
-			catch
-			{
-			    return false;
-			}
-			return true;
-		    }
+                    bool IsInt(string s)
+                    {
+                        try
+                        {
+                            int.Parse(s);
+                        }
 
-		    bool IsBool(string s)
-		    {
-			try
-			{
-			    bool.Parse(s);
-			}
+                        catch
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
 
-			catch
-			{
-			    return false;
-			}
-			return true;
-		    }
-		    
-		    if (sections[0] == string.Empty || sections[1] == string.Empty)
-		    {
-			yield return null;
-			yield break;
-		    }
+                    bool IsBool(string s)
+                    {
+                        try
+                        {
+                            bool.Parse(s);
+                        }
 
-		    if (!IsDouble(sections[2]) || !IsDouble(sections[3]) || !IsDouble(sections[6]))
-		    {
-			yield return null;
-			yield break;
-		    }
+                        catch
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
 
-		    if (!IsBool(sections[4]) || !IsBool(sections[5]))
-		    {
-			yield return null;
-			yield break;
-		    }
+                    if (sections[0] == string.Empty || sections[1] == string.Empty)
+                    {
+                        yield return null;
+                        yield break;
+                    }
 
-		    if (!IsInt(sections[7]))
-		    {
-			yield return null;
-			yield break;
-		    }
-		    
-		    yield return sections;
-		}
-	    }
+                    if (!IsDouble(sections[2]) || !IsDouble(sections[3]) || !IsDouble(sections[6]))
+                    {
+                        yield return null;
+                        yield break;
+                    }
 
-	    public WeedStrain[] GetStrains()
-	    {
-		List<WeedStrain> strains = new List<WeedStrain>();
+                    if (!IsBool(sections[4]) || !IsBool(sections[5]))
+                    {
+                        yield return null;
+                        yield break;
+                    }
 
-		try
-		{
-		    if (!File.Exists("strains.dash"))
-		    {
-			return null;
-		    }
+                    if (!IsInt(sections[7]))
+                    {
+                        yield return null;
+                        yield break;
+                    }
 
-		    var blocks = TranslateHerbBlocks();
+                    yield return sections;
+                }
+            }
 
-		    if (blocks == null)
-		    {
-			return null;
-		    }
-		    
-		    foreach (string[] block in blocks)
-		    {
-			strains.Add
-			(
-			    new WeedStrain()
-			    {
-				SativaContents = double.Parse(block[2]),
-				IndicaContents = double.Parse(block[3]),
-				Price = double.Parse(block[6]),
-				Autoflower = bool.Parse(block[4]),
-				Feminised = bool.Parse(block[5]),
-				Weeks = int.Parse(block[7]),
-				StrainName = block[0],
-				Crossings = block[1],
-			    }
-			);
-		    }
-		}
+            public WeedStrain[] GetStrains()
+            {
+                List<WeedStrain> strains = new List<WeedStrain>();
 
-		catch
-		{
-		    throw;
-		}
-		
-		return strains.ToArray();
-	    }
-	}
+                try
+                {
+                    if (!File.Exists("strains.dash"))
+                    {
+                        return null;
+                    }
+
+                    var blocks = TranslateHerbBlocks();
+
+                    if (blocks == null)
+                    {
+                        return null;
+                    }
+
+                    foreach (string[] block in blocks)
+                    {
+                        strains.Add
+                        (
+                            new WeedStrain()
+                            {
+                                SativaContents = double.Parse(block[2]),
+                                IndicaContents = double.Parse(block[3]),
+                                Price = double.Parse(block[6]),
+                                Autoflower = bool.Parse(block[4]),
+                                Feminised = bool.Parse(block[5]),
+                                Weeks = int.Parse(block[7]),
+                                StrainName = block[0],
+                                Crossings = block[1],
+                            }
+                        );
+                    }
+                }
+
+                catch
+                {
+                    throw;
+                }
+
+                return strains.ToArray();
+            }
+        }
     }
 }
