e89f6ce4fbdf596adf2dad9facf2993db37812ae
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-15 11:34:27 +0300

    reformat
    Reformat of the file, because VS2019 hates my indentations.  Also, inactivity was due to study, obtaining information tends to be more useful than expressing it.  Regardless, this is one of the many commits.  Many files were reformatted.  RIP me.

--- Framework/class/b/NETExt/control.cs
+++ Framework/class/b/NETExt/control.cs
@@ -12,58 +12,58 @@ namespace DashFramework
 {
     namespace NETExtensions
     {
-	public static class ControlExtension
-	{
-	    readonly static Extern ext = new Extern();
+        public static class ControlExtension
+        {
+            readonly static Extern ext = new Extern();
 
-	    public static void SetUrl(this Control control, string dest)
-	    {
-		try
-		{
-		    control.Click += (s, e) =>
-		    {
-			try
-			{
-			    ext.OpenUrl(dest);
-			}
+            public static void SetUrl(this Control control, string dest)
+            {
+                try
+                {
+                    control.Click += (s, e) =>
+                    {
+                        try
+                        {
+                            ext.OpenUrl(dest);
+                        }
 
-			catch
-			{
-			    throw;
-			}
-		    };
-		}
+                        catch
+                        {
+                            throw;
+                        }
+                    };
+                }
 
-		catch
-		{
-		    throw;
-		}
-	    }
+                catch
+                {
+                    throw;
+                }
+            }
 
 
-	    public static void RegisterClickEvent(this Control control, Action code)
-	    {
-		try
-		{
-		    control.Click += (s, e) =>
-		    {
-			try
-			{
-			    code();
-			}
+            public static void RegisterClickEvent(this Control control, Action code)
+            {
+                try
+                {
+                    control.Click += (s, e) =>
+                    {
+                        try
+                        {
+                            code();
+                        }
 
-			catch
-			{
-			    throw;
-			}
-		    };
-		}
+                        catch
+                        {
+                            throw;
+                        }
+                    };
+                }
 
-		catch
-		{
-		    throw;
-		}
-	    }
-	}
+                catch
+                {
+                    throw;
+                }
+            }
+        }
     }
 }
--- Framework/class/b/NETExt/string.cs
+++ Framework/class/b/NETExt/string.cs
@@ -9,22 +9,22 @@ namespace DashFramework
 {
     namespace NETExtensions
     {
-	public static class StringExtension
-	{
-	    public static string[] ISplit(this string inst, params char[] chars)
-	    {
-		return inst.Split(chars, StringSplitOptions.RemoveEmptyEntries);
-	    }
+        public static class StringExtension
+        {
+            public static string[] ISplit(this string inst, params char[] chars)
+            {
+                return inst.Split(chars, StringSplitOptions.RemoveEmptyEntries);
+            }
 
-	    public static string RemoveAll(this string inst, params string[] crits)
-	    {
-		foreach (string crit in crits)
-		{
-		    inst = inst.Replace(crit, "");
-		}
+            public static string RemoveAll(this string inst, params string[] crits)
+            {
+                foreach (string crit in crits)
+                {
+                    inst = inst.Replace(crit, "");
+                }
 
-		return inst;
-	    }
-	}
+                return inst;
+            }
+        }
     }
 }
