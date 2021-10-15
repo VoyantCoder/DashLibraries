d9381a80c9084143ec9333405fc3ed9ae45a7cc0
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-15 11:41:53 +0300

    null-control caching
    Makes me able to add controls without giving my method an object.  This is handy if you want to add 10 buttons but do not want to declare 10 button objects, or any at all.

---
+++ Framework/class/b/Windows/Controls/Integration/Caching/Controls.cs
@@ -0,0 +1,54 @@
+﻿
+// Author: Dashie
+
+
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+
+namespace DashFramework
+{
+    namespace DashControls
+    {
+        public partial class ControlIntegrator
+        {
+            private void Register(Control parent, Control child)
+            {
+                try
+                {
+                    if (!Controls.ContainsKey(parent))
+                    {
+                        Controls.Add(parent, new List<Control>());
+                    }
+
+                    Controls[parent].Add(child);
+                }
+
+                catch
+                {
+                    throw;
+                }
+            }
+
+            private void UpdateRegister(Control parent, Control control)
+            {
+                try
+                {
+                    if (Controls.ContainsKey(parent))
+                    {
+                        if (Controls[parent].Contains(control))
+                        {//only works for first occurrence, but why occur twice?
+                            int id = Controls[parent].IndexOf(control);
+                            Controls[parent][id] = control;
+                        }
+                    }
+                }
+
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
