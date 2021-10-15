ded590afe5448648aef7d76d418c1c0769f8005d
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-15 11:34:52 +0300

    reformat
    Reformat of the file, because VS2019 hates my indentations.  Also, inactivity was due to study, obtaining information tends to be more useful than expressing it.  Regardless, this is one of the many commits.  Many files were reformatted.  RIP me.

--- Framework/class/b/Networking/Ports/PortScan.cs
+++ Framework/class/b/Networking/Ports/PortScan.cs
@@ -13,132 +13,132 @@ namespace DashFramework
 {
     namespace Networking
     {
-	public partial class Network
-	{
-	    public partial class PortScanner : Network
-	    {
-		public IEnumerable<(int, bool)> ScanPorts(string host, int[] ports, PSFormatType PortFormatType, int timeout = 1000)
-		{
-		    (int, bool) exit = (-1, false);
+        public partial class Network
+        {
+            public partial class PortScanner : Network
+            {
+                public IEnumerable<(int, bool)> ScanPorts(string host, int[] ports, PSFormatType PortFormatType, int timeout = 1000)
+                {
+                    (int, bool) exit = (-1, false);
 
-		    if (DoCheckAddressFormat)
-		    {
-			if (!IsIp(host))
-			{
-			    yield return exit;
-			    yield break;
-			}
-		    }
+                    if (DoCheckAddressFormat)
+                    {
+                        if (!IsIp(host))
+                        {
+                            yield return exit;
+                            yield break;
+                        }
+                    }
 
-		    if (DoCheckTimeout)
-		    {
-			if (!IsDuration(timeout))
-			{
-			    yield return exit;
-			    yield break;
-			}
-		    }
+                    if (DoCheckTimeout)
+                    {
+                        if (!IsDuration(timeout))
+                        {
+                            yield return exit;
+                            yield break;
+                        }
+                    }
 
-		    if (DoCheckPortFormat)
-		    {
-			if (ports == null || ports.Length < 1)
-			{
-			    yield return exit;
-			    yield break;
-			}
-		    }
+                    if (DoCheckPortFormat)
+                    {
+                        if (ports == null || ports.Length < 1)
+                        {
+                            yield return exit;
+                            yield break;
+                        }
+                    }
 
-		    switch (PortFormatType)
-		    {
-			case PSFormatType.Specific://specific
-			    foreach (var value in Hook1())
-				yield return value;
-			    break;
-			case PSFormatType.Ranged://range
-			    foreach (var value in Hook2())
-				yield return value;
-			    break;
-			case PSFormatType.Singular://single
-			    yield return Hook3();
-			    break;
-		    }
+                    switch (PortFormatType)
+                    {
+                        case PSFormatType.Specific://specific
+                            foreach (var value in Hook1())
+                                yield return value;
+                            break;
+                        case PSFormatType.Ranged://range
+                            foreach (var value in Hook2())
+                                yield return value;
+                            break;
+                        case PSFormatType.Singular://single
+                            yield return Hook3();
+                            break;
+                    }
 
-		    host = GetIp(host).ToString();
+                    host = GetIp(host).ToString();
 
-		    bool SocketStatus(Socket socket)
-		    {
-			return (socket == null ? false : true);
-		    }
+                    bool SocketStatus(Socket socket)
+                    {
+                        return (socket == null ? false : true);
+                    }
 
-		    IEnumerable<(int, bool)> Hook1()
-		    {
-			foreach (int port in ports)
-			{
-			    Socket socket = SocketConnect(host, port, timeout);
-			    bool result = SocketStatus(socket);
-			    yield return (port, result);
-			}
-		    }
+                    IEnumerable<(int, bool)> Hook1()
+                    {
+                        foreach (int port in ports)
+                        {
+                            Socket socket = SocketConnect(host, port, timeout);
+                            bool result = SocketStatus(socket);
+                            yield return (port, result);
+                        }
+                    }
 
-		    IEnumerable<(int, bool)> Hook2()
-		    {
-			if (ports.Length != 2 || ports[0] >= ports[1])
-			{
-			    yield return exit;
-			    yield break;
-			}
-			
-			for (int p = ports[0]; p <= ports[1]; p += 1)
-			{
-			    Socket socket = SocketConnect(host, p, timeout);
-			    bool result = SocketStatus(socket);
-			    yield return (p, result);
-			}
-		    }
+                    IEnumerable<(int, bool)> Hook2()
+                    {
+                        if (ports.Length != 2 || ports[0] >= ports[1])
+                        {
+                            yield return exit;
+                            yield break;
+                        }
 
-		    (int, bool) Hook3()
-		    {
-			try
-			{
-			    Socket socket = SocketConnect(host, ports[0], timeout);
-			    bool result = SocketStatus(socket);
-			    return (ports[0], result);
-			}
+                        for (int p = ports[0]; p <= ports[1]; p += 1)
+                        {
+                            Socket socket = SocketConnect(host, p, timeout);
+                            bool result = SocketStatus(socket);
+                            yield return (p, result);
+                        }
+                    }
 
-			catch
-			{
-			    throw;
-			}
-		    }
-		}
+                    (int, bool) Hook3()
+                    {
+                        try
+                        {
+                            Socket socket = SocketConnect(host, ports[0], timeout);
+                            bool result = SocketStatus(socket);
+                            return (ports[0], result);
+                        }
 
-		public IEnumerable<(string, IEnumerable<(int, bool)>)> MultiScan(string[] hosts, int[] ports, PSFormatType PortFormatType, int timeout = 1000)
-		{
-		    foreach (string host in hosts)
-		    {
-			IEnumerable<(int, bool)> result = ScanPorts
-			    (host, ports, PortFormatType, timeout);
+                        catch
+                        {
+                            throw;
+                        }
+                    }
+                }
 
-			yield return (host, result);
-		    }
-		}
+                public IEnumerable<(string, IEnumerable<(int, bool)>)> MultiScan(string[] hosts, int[] ports, PSFormatType PortFormatType, int timeout = 1000)
+                {
+                    foreach (string host in hosts)
+                    {
+                        IEnumerable<(int, bool)> result = ScanPorts
+                            (host, ports, PortFormatType, timeout);
 
-		public bool ScanPort(string host, int port, int timeout = 1000)
-		{
-		    try
-		    {
-			(int, bool) result = ScanPorts(host, new int[] { port },
-			    PSFormatType.Singular, timeout).ToList()[0];
+                        yield return (host, result);
+                    }
+                }
 
-			return result.Item2;
-		    }
+                public bool ScanPort(string host, int port, int timeout = 1000)
+                {
+                    try
+                    {
+                        (int, bool) result = ScanPorts(host, new int[] { port },
+                            PSFormatType.Singular, timeout).ToList()[0];
 
-		    catch
-		    {
-			return false;
-		    }
-		}
-	    }
-	}
+                        return result.Item2;
+                    }
+
+                    catch
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
     }
 }
--- Framework/class/b/Networking/Ports/Settings.cs
+++ Framework/class/b/Networking/Ports/Settings.cs
@@ -9,68 +9,68 @@ namespace DashFramework
 {
     namespace Networking
     {
-	public enum PSFormatType
-	{
-	    Singular, Ranged, Specific
-	}
+        public enum PSFormatType
+        {
+            Singular, Ranged, Specific
+        }
 
-	public enum PSSettingsType
-	{
-	    Address, Port, Timeout, Packets
-	}
+        public enum PSSettingsType
+        {
+            Address, Port, Timeout, Packets
+        }
 
-	public partial class Network
-	{
-	    public partial class PortScanner
-	    {
-		public bool DoCheckAddressFormat = true;
-		public bool DoCheckPortFormat = true;
-		public bool DoCheckTimeout = true;
-		public bool SendPacketOnConnect = false;
-		
-		public void ToggleSetting(PSSettingsType Setting)
-		{
-		    try
-		    {
-			switch (Setting)
-			{
-			    case PSSettingsType.Address:
-			    {
-				DoCheckAddressFormat =
-				    (DoCheckAddressFormat == true ? false : true);
+        public partial class Network
+        {
+            public partial class PortScanner
+            {
+                public bool DoCheckAddressFormat = true;
+                public bool DoCheckPortFormat = true;
+                public bool DoCheckTimeout = true;
+                public bool SendPacketOnConnect = false;
 
-				break;
-			    }
-			    case PSSettingsType.Packets:
-			    {
-				SendPacketOnConnect =
-				    (SendPacketOnConnect == true ? false : true);
+                public void ToggleSetting(PSSettingsType Setting)
+                {
+                    try
+                    {
+                        switch (Setting)
+                        {
+                            case PSSettingsType.Address:
+                                {
+                                    DoCheckAddressFormat =
+                                        (DoCheckAddressFormat == true ? false : true);
 
-				break;
-			    }
-			    case PSSettingsType.Timeout:
-			    {
-				DoCheckTimeout =
-				    (DoCheckTimeout == true ? false : true);
+                                    break;
+                                }
+                            case PSSettingsType.Packets:
+                                {
+                                    SendPacketOnConnect =
+                                        (SendPacketOnConnect == true ? false : true);
 
-				break;
-			    }
-			    case PSSettingsType.Port:
-			    { 
-				DoCheckPortFormat =
-				    (DoCheckPortFormat == true ? false : true);
+                                    break;
+                                }
+                            case PSSettingsType.Timeout:
+                                {
+                                    DoCheckTimeout =
+                                        (DoCheckTimeout == true ? false : true);
 
-				break;
-			    }
-			}
-		    }
+                                    break;
+                                }
+                            case PSSettingsType.Port:
+                                {
+                                    DoCheckPortFormat =
+                                        (DoCheckPortFormat == true ? false : true);
 
-		    catch
-		    {
-			throw;
-		    }
-		}
-	    }
-	}
+                                    break;
+                                }
+                        }
+                    }
+
+                    catch
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
     }
 }
