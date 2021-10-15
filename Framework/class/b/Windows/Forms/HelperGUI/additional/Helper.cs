052bc6a953b44117fe783811c00b4cd36f44c47b
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-15 11:42:14 +0300

    Show/Hide added

---
+++ Framework/class/b/Windows/Forms/HelperGUI/additional/Helper.cs
@@ -0,0 +1,43 @@
+﻿
+// Author: Dashie
+
+
+using System.Windows.Forms;
+
+using DashFramework.Forms;
+
+
+namespace DashFramework
+{
+    namespace Forms
+    {
+        public partial class HelperGUI
+        {
+            public void Show()
+            {
+                try
+                {
+                    WindowInst.Show();
+                }
+
+                catch
+                {
+                    throw;
+                }
+            }
+
+            public void Hide()
+            {
+                try
+                {
+                    WindowInst.Hide();
+                }
+
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
