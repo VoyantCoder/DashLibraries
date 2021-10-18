2a514ee1ee736a7d43f5365bcd901da31ad4e693
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-18 20:58:28 +0300

    fixed border thickness

--- Framework/class/b/Windows/Forms/DashWindow/Bordering.cs
+++ Framework/class/b/Windows/Forms/DashWindow/Bordering.cs
@@ -11,34 +11,16 @@ namespace DashFramework
     {
         public partial class DashWindow
         {
-            public MenuBar Menubar()
-            {
-                try
-                {
-                    if (MenubarInstance.HasMenubarBeenAdded())
-                    {
-                        return MenubarInstance;
-                    }
-
-                    return null;
-                }
-
-                catch
-                {
-                    throw;
-                }
-            }
-
             public void DrawWindowBorder(Color Color, int Thickness)
             {
                 try
                 {
-                    Size Size = new Size(WindowInstance.Width - (Thickness - 1), WindowInstance.Height - (Thickness - 1));
-                    Point Location = new Point(0, 0);
+                    Size Size = new Size(WindowInstance.Width - (Thickness + 1), WindowInstance.Height - (Thickness - 1));
+                    Point Location = new Point(1, 0);
 
-                    if (Menubar() != null)
+                    if (Menubar != null)
                     {
-                        Location.Y = Menubar().GetMenubarSize().Height + Menubar().GetMenubarLocation().Y;
+                        Location.Y = Menubar.GetMenubarSize().Height + Menubar.GetMenubarLocation().Y;
                     }
 
                     Transform.PaintRectangle(WindowInstance, Thickness, Size, Location, Color);
