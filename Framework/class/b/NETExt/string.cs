b0f70427041e7a01d123ee9fba31a64060ac4a99
KvinneKraft <KvinneKraft@protonmail.com>
2021-10-21 23:44:01 +0300

    rid spaces extension

--- Framework/class/b/NETExt/string.cs
+++ Framework/class/b/NETExt/string.cs
@@ -9,6 +9,11 @@ namespace DashFramework
 {
     namespace NETExtensions
     {
+        public enum RidSpaceMode
+        {
+            End, Start, Double, All
+        }
+
         public static class StringExtension
         {
             public static string[] ISplit(this string inst, params char[] chars)
@@ -24,6 +29,90 @@ namespace DashFramework
                 }
 
                 return inst;
+            }
+
+            public static string RidSpaces(this string inst, RidSpaceMode mode)
+            {
+                try
+                {
+                    void Double()
+                    {
+                        try
+                        {
+                            while (inst.Contains("  "))
+                            {
+                                inst = inst.Replace("  ", " ");
+                            }
+                        }
+
+                        catch
+                        {
+                            throw;
+                        }
+                    }
+
+                    void Start()
+                    {
+                        try
+                        {
+                            while (inst.StartsWith(" "))
+                            {
+                                inst = inst.Remove(0, 1);
+                            }
+                        }
+
+                        catch
+                        {
+                            throw;
+                        }
+                    }
+
+                    void End()
+                    {
+                        try
+                        {
+                            while (inst.EndsWith(" "))
+                            {
+                                inst = inst.Remove(inst.Length - 1, 1);
+                            }
+                        }
+
+                        catch
+                        {
+                            throw;
+                        }
+                    }
+
+                    void All()
+                    {
+                        try
+                        {
+                            Start();
+                            End();
+                            Double();
+                        }
+                        
+                        catch
+                        {
+                            throw;
+                        }
+                    }
+
+                    switch (mode)
+                    {
+                        case RidSpaceMode.Double: Double(); break;
+                        case RidSpaceMode.Start: Start(); break;
+                        case RidSpaceMode.End: End();  break;
+                        case RidSpaceMode.All: All(); break;
+                    }
+
+                    return inst;
+                }
+
+                catch
+                {
+                    throw;
+                }
             }
         }
     }
